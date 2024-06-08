70a50b457d4a96cfcd235fcf02aa64a70c5d76d2
Alexkjl <109453797+kohjlalex@users.noreply.github.com>
2024-06-08 13:39:39 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def menu(show_select_msg=False):
     with st.sidebar:
-        st.page_link("welcome.py", label="Home", icon="🏠❤️")
+        st.page_link("Main.py", label="Home", icon="🏠❤️")
         st.page_link("pages/ebay-tele-notifications.py", label="Your Ebay Telegram Notifications Bot", icon="🛒")
         if show_select_msg:
             st.success("Select a demo above.")
