21e0e9409553d01fd7530ca95156c89fbeb5affe
Alexkjl <109453797+kohjlalex@users.noreply.github.com>
2024-06-08 14:17:03 +0800

    Update 1_🚀_stocks-automation.py

--- pages/1_🚀_stocks-automation.py
+++ pages/1_🚀_stocks-automation.py
@@ -1,3 +1,75 @@
-import streamlit 
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+import pendulum
+import mplfinance as mpf
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import json
+import requests
+from bs4 import BeautifulSoup
 
-streamlit.text("Hello")
+# Function to create the form
+def user_input():
+    st.title("Stock Ticker Input Form")
+    
+    # Text input for API key
+    api_key = st.text_input("Enter your API key", type="password")  # type="password" hides the input
+
+    # Text input for Stock ticker
+    ticker = st.text_input("Enter the stock ticker")
+
+    # Display the input values
+    if st.button("Submit"):
+        if api_key and ticker:
+            #st.write(f"API Key: {api_key}")
+            st.write(f"Stock Ticker: {ticker}")
+        else:
+            st.write("Please fill in both fields.")
+
+file_paths = ['income_statement.json', 'balance_sheet.json', 'cash_flow.json']
+
+def process_api_request(api_key, ticker):
+  #Income Statement
+  financial_statement = "INCOME_STATEMENT"
+  url_income_statement = "https://www.alphavantage.co/query?function="+ financial_statement + "&symbol="+ ticker + "&apikey="+ alpha_vantage_api_key
+
+  #Balance Sheet
+  financial_statement = "BALANCE_SHEET"
+  url_balance_sheet = "https://www.alphavantage.co/query?function="+ financial_statement + "&symbol="+ ticker + "&apikey="+ alpha_vantage_api_key
+
+  #Cash Flow
+  financial_statement = "CASH_FLOW"
+  url_cash_flow = "https://www.alphavantage.co/query?function="+ financial_statement + "&symbol="+ ticker + "&apikey="+ alpha_vantage_api_key
+
+  # URLs and file paths
+  urls = [url_income_statement, url_balance_sheet, url_cash_flow]
+
+  # Dictionary to store the URL to file path mapping
+  url_to_file_path = dict(zip(urls, file_paths))
+
+  data_income_statement, data_balance_sheet, data_cash_flow = {}, {}, {}
+
+  # Save JSON responses to files
+  for url, file_path in url_to_file_path.items():
+      response = requests.get(url)
+      json_data = response.json()
+
+      with open(file_path, 'w') as file:
+          json.dump(json_data, file, indent=4)  # Correctly dumping JSON data
+
+      print(f"JSON data from {url} has been written to {file_path}")
+      # Assign data to respective variables
+      if "INCOME_STATEMENT" in url:
+          data_income_statement = json_data
+      elif "BALANCE_SHEET" in url:
+          data_balance_sheet = json_data
+      elif "CASH_FLOW" in url:
+          data_cash_flow = json_data
+
+  return (data_income_statement, data_balance_sheet, data_cash_flow)
+
+
+# Main function to call the user input function
+if __name__ == "__main__":
+    user_input()
