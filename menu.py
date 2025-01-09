6f2c3ee65f8f852765acd5770b16466811d3b66f
Alexkjl <109453797+kohjlalex@users.noreply.github.com>
2025-01-09 15:41:05 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ import streamlit as st
 def menu(show_select_msg=False):
     with st.sidebar:
         st.page_link("Main.py", label="Home", icon="🏠"),
-        st.page_link("pages/ebay-tele-notifications.py", label="Your Ebay Telegram Notifications Bot", icon="🛒")
+        #st.page_link("pages/ebay-tele-notifications.py", label="Your Ebay Telegram Notifications Bot", icon="🛒")
         st.page_link("pages/1_🚀_stocks_automation.py", label="Stocks Health Screener", icon="🚀")
         st.page_link("pages/2_👷_automatic_filter.py", label="Web Scraper & PDF Manipulation", icon="👷")
         if show_select_msg:
